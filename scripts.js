2ae2c404fc09f75ee5a9b5c2f67e0b9b0a212f29
adamShaleen <adamshaleen@gmail.com>
2016-11-25 16:34:35 -0600

    added fadein, favicon, and some style.

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   var currentAuthor = "";
   $("#generateQuoteButton").on("click", function() {
     $.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json?dataType=%27jsonp%27&type=%27GET%27", function(json) {
-      $("#quote").html(JSON.stringify(json.quote));
-      $("#quoteAuthor").html(json.author);
+      $("#quote").html(JSON.stringify(json.quote)).hide().fadeIn("slow");
+      $("#quoteAuthor").html(json.author).hide().fadeIn("slow");
       currentQuote = json.quote;
       currentAuthor = json.author;
     });
   });
-$('#generateTwitterButton').on("click", function() {
-  window.open("https://twitter.com/intent/tweet?text="+ currentQuote + " -" + currentAuthor);
-});  
+  $('#generateTwitterButton').on("click", function() {
+      window.open("https://twitter.com/intent/tweet?text="+ currentQuote + " -" + currentAuthor);
+  });
 });
