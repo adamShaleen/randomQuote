41af0262ce95d1664b95921c850bdbf7bec65710
adamShaleen <adamshaleen@gmail.com>
2016-11-24 22:03:01 -0600

    added twitter functionality

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
+  var currentQuote = "";
+  var currentAuthor = "";
   $("#generateQuoteButton").on("click", function() {
     $.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json?dataType=%27jsonp%27&type=%27GET%27", function(json) {
       $("#quote").html(JSON.stringify(json.quote));
       $("#quoteAuthor").html(json.author);
+      currentQuote = json.quote;
+      currentAuthor = json.author;
     });
   });
+$('#generateTwitterButton').on("click", function() {
+  window.open("https://twitter.com/intent/tweet?text="+ currentQuote + " -" + currentAuthor);
+});  
 });
