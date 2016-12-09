53b1b4b540ffbdd2f0a1c4da4edba9c8fcfc36b4
adamShaleen <adamshaleen@gmail.com>
2016-12-08 19:37:29 -0600

    changed to a different API and removed crossorigin.me

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+
   // global variable to store current quote/author from API
   var currentQuote = "";
   var currentAuthor = "";
+
   // list of background colors
   var colors = ["rgb(219, 193, 251)", "rgb(145, 177, 109)", "rgb(238, 158, 83)", "rgb(227, 64, 137)", "rgb(52, 100, 246)"];
+
   // function to increment through colors list
   var i = 0;
   function changeColor() {
@@ -14,15 +17,21 @@ $(document).ready(function() {
   }
   // When quote button is clicked, use proxy to call quote API with getJSON request, fade in quote/author and render to corresponding element.
   $("#generateQuoteButton").on("click", function() {
+
     changeColor();
+
     $("body").animate({backgroundColor: colors[i]}, 1000);
-    $.getJSON("https://crossorigin.me/https://quotes.stormconsultancy.co.uk/random.json?dataType=%27jsonp%27&type=%27GET%27", function(json) {
+
+    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json", function(json) {
+
       $("#quote").html(JSON.stringify(json.quote)).hide().fadeIn(800);
       $("#quoteAuthor").html(json.author).hide().fadeIn(800);
+
       // set the quote/author from the quote on the screen.
       currentQuote = json.quote;
       currentAuthor = json.author;
     });
+
   });
   // When twitter button is clicked open a new window with twitter dashboard, loaded with current quote/author.
   $('#generateTwitterButton').on("click", function() {
